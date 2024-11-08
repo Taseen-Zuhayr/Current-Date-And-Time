f97adee597d30cda676977a9b94cca1f019d568c
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-11-08 22:13:51 +0600

    Currant Date and Time

---
+++ main.py
@@ -0,0 +1,11 @@
+from datetime import date, time, datetime
+
+today = date.today()
+now = datetime.now()
+
+print("Today is ",today)
+print("Date and time is ",now)
+print("The currant month is ",today.month)
+print("The currant year is ",today.year)
+print("The currant day is ",today.day)
+
